e909109b18acb818c00c4c29dbbeed4051ac6589
Guido Scialfa <dev@guidoscialfa.com>
2018-02-17 17:52:55 +0100

    Move `data` method from the template interface into the data interface.

--- src/TemplateInterface.php
+++ src/TemplateInterface.php
@@ -36,18 +36,6 @@ namespace TemplateLoader;
 interface TemplateInterface
 {
     /**
-     * Template Data
-     *
-     * The stdClass object to use as container for the view data.
-     *
-     * @since 2.1.0
-     * @since 4.0.0 The returned value is a \TemplateLoader\DataInterface.
-     *
-     * @return DataInterface A instance that contain the view data properties
-     */
-    public function data();
-
-    /**
      * Template
      *
      * @since  2.1.0
